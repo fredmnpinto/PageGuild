a78839b279683eac333cf71777749ff807e40c5f
Gabriel Fernandes <force2606@gmail.com>
2021-12-04 21:12:05 +0000

    Configuração inicial do Laravel

---
+++ PageGuild/app/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PageGuild;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("index");
+    }
+}
---
+++ PageGuild/resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+Laravel Programming Lab. | Index Page
+@endsection
+
+@section('content')
+<h2 class="mt-5 mb-3 text-center">Laravel Programming Lab.!</h2>
+@endsection
