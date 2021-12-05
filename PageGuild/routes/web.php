90f5944cfbcbadaa3c0b3b92c13c4cdf7ab312b5
Frederico Pinto <fredmnpinto@gmail.com>
2021-12-05 18:58:10 +0000

    Config FRED

--- PageGuild/routes/web.php
+++ PageGuild/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [IndexController::Class, 'index']);
 
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/index', [IndexController::Class, 'index']);
