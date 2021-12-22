700689d5247868894e7bb515c91a83a10f9f60b9
Gabriel Fernandes <force2606@gmail.com>
2021-12-22 00:05:41 +0000

    Create ratingController

---
+++ PageGuild/app/Http/Controllers/RatingController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RatingController extends Controller
+{
+    /**
+     * Creates a rating on Database
+     */
+    public function create() {
+
+    }
+
+    /**
+     * Reads a rating from database
+     * 
+     * @param int $id
+     */
+    public function read($id) {
+
+    }
+
+    /**
+     * Updates a rating in the database
+     * 
+     * @param int $id
+     */
+    public function update($id) {
+
+    }
+
+    /**
+     * Deletes a rating in the database
+     * 
+     * @param int $id
+     */
+    public function delete($id) {
+
+    }
+}
