6813a937a1a6cd3846b3ca318a4ed03644a5e2a4
Frederico Pinto <39822@ufp.edu.pt>
2021-12-30 14:21:29 +0000

    Automated tests for Registration

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Feature\Auth;
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Models\User;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+
+    public function test_index() {
+        $this->get('/')->assertStatus(200);
+    }
+
+    public function test_registration()
+    {
+        /* User to be tested */
+        $testUser = [
+            'username' => 'testusername',
+            'password' => 'testpassword',
+            'email' => 'testemail@example.com',
+        ];
+
+        /* Make sure it doesn't already exists */
+        $user_id = User::where('username', '=', $testUser['username'])
+            ->where('email', '=', $testUser['email'])
+            ->get('id')[0]['id'];
+
+        if ($user_id != null) {
+            User::destroy(
+                $user_id
+            );
+        }
+
+        $this->assertDatabaseMissing(User::class, [
+            'username' => $testUser['username'],
+            'email' => $testUser['email'],
+        ]);
+
+        /* Register that user */
+        $registerResponse = $this
+            ->post('/register',
+                [
+                    'name' => 'Test User',
+                    'username' => $testUser['username'],
+                    'email' => $testUser['email'],
+                    'password' => $testUser['password'],
+                    'password_confirmation' => $testUser['password'],
+                    'sex' => 'male',
+                ]);
+
+        $registerResponse->assertRedirect('/home');
+
+        /* Make sure the user now exists in the Database */
+        $this->assertDatabaseHas(User::class, [
+            'username' => $testUser['username'],
+            'email' => $testUser['email'],
+        ]);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_login_registered_user()
+    {
+        /* Register the testuser */
+        $this->test_registration();
+
+        /* User to be tested */
+        $testUser = [
+            'username' => 'testusername',
+            'password' => 'testpassword',
+            'email' => 'testemail@example.com',
+        ];
+
+        /* Try to login with him */
+        $loginResponse = $this
+            ->post('/login', [
+                'username' => $testUser['username'],
+                'password' => $testUser['password'],
+            ]);
+
+        $loginResponse->assertRedirect('/home');
+    }
+}
